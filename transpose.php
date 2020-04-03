9e93f3b3e578094606e6239c4ab628d4a5fe7fa5
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-03 22:53:02 +0530

    Update transpose.php

--- transpose.php
+++ transpose.php
@@ -32,3 +32,28 @@ for ($col = 0; $col < $n; $col++)
 echo " ".$a[$col][$row];
 echo "<br/>";
 }
+if(($m===$p) and ($n===$q)) {
+echo "the addition of matrices is:"."<br/>";
+for ($row = 0; $row < 3; $row++) {
+for ($col = 0; $col < 3; $col++)
+echo " ".$a[$row][$col]+$b[$row][$col]." ";
+echo "<br/>";
+}
+}
+if($n===$p){
+echo " The multiplication of matrices: <br/>";
+$result=array();
+for ($i=0; $i < $m; $i++) {
+for($j=0; $j < $q; $j++){
+$result[$i][$j] = 0;
+for($k=0; $k < $n; $k++)
+$result[$i][$j] += $a[$i][$k] * $b[$k][$j];
+}
+}
+for ($row = 0; $row < $m; $row++) {
+for ($col = 0; $col < $q; $col++)
+echo " ".$result[$row][$col];
+echo "<br/>";
+}
+}
+?>
