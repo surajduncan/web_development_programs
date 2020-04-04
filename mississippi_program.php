df19d0948787e5ba73b9fec6e4aeaf8b37168981
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-04 22:31:35 +0530

    Create mississippi_program.php

---
+++ mississippi_program.php
@@ -0,0 +1,4 @@
+Write a PHP program named states.py that declares a variable states with value “Mississippi
+Alabama Texas Massachusetts Kansas". write a PHP program that does the following:
+a) Search for a word in variable states that ends in xas. Store this word in element0 of a list
+named states List.
