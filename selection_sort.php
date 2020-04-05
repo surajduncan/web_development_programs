8cec4ee311df4395248f11cef391776573c32877
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-05 22:32:32 +0530

    Create selection_sort.php

---
+++ selection_sort.php
@@ -0,0 +1,14 @@
+# Write a PHP program to sort the student records which are stored in the
+database using selection sort.
+
+
+
+
+
+
+
+
+Goto Mysql and then type
+create database weblab;
+use weblab;
+create table student(usnvarchar(10),name varchar(20),address varchar(20));
