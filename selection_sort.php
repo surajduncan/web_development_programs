14ef9fc67481cc683b7d107b56f0ab51a5868353
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-05 22:33:04 +0530

    Update selection_sort.php

--- selection_sort.php
+++ selection_sort.php
@@ -12,3 +12,31 @@ Goto Mysql and then type
 create database weblab;
 use weblab;
 create table student(usnvarchar(10),name varchar(20),address varchar(20));
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+table, td, th
+{
+border: 1px solid black;
+width: 33%;
+text-align: center;
+border-collapse:collapse;
+background-color:lightblue;
+}
+table { margin: auto; }
+</style>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "weblab";
+$a=[];
+// Create connection
+// Opens a new connection to the MySQL server
+$conn = mysqli_connect($servername, $username, $password, $dbname);
